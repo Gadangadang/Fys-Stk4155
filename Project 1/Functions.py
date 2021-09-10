1013366fc3450500bfca4aee6065edfe0673a663
William Hirst <bod_holthe@outlook.com>
2021-09-10 15:21:08 +0200

    Created Functions.py
    This will be a file with all essential functions.

---
+++ Project 1/Functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def R2(y_data, y_model):
+    return 1 - np.sum((y_data - y_model)**2) / np.sum((y_data - np.mean(y_data)) ** 2)
+
+#Mean sqaured error
+def MSE(y_data,y_model):
+    n = np.size(y_model)
+    return np.sum((y_data-y_model)**2)/n
+
+
+def FrankeFunction(x,y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
