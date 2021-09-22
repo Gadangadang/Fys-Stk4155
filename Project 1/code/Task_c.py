491cde6b89d028da46a6d73f1e5cfe415bbaa093
Gadangadang <sakarias.frette@outlook.com>
2021-09-22 15:39:14 +0200

    Added new code file for task 3

---
+++ Project 1/code/Task_c.py
@@ -0,0 +1,6 @@
+import numpy as np
+from random import random, seed
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from scipy.stats import norm
+from Functions import *
