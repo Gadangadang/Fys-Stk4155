712c1c481624a02098f6f93608580b81c4ffd55c
William Hirst <bod_holthe@outlook.com>
2021-09-22 16:02:21 +0200

    Started making cross_validation method.
    Not yet tested or completed.

--- Project 1/code/Task_c.py
+++ Project 1/code/Task_c.py
@@ -1,15 +1,34 @@
 import numpy as np
 from random import random, seed
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from scipy.stats import norm
 from Functions import *
 
 
-def cross_validation(X_train, X_test, z_train, z_test, k_fold_number):
+def cross_validation(N, z_noise, k_fold_number, n):
+    """
+    
+    """
+    x, y, z = generate_data(N, z_noise, seed=2018)
+    X = create_X(x, y, n)
+    kfold = KFold(n_splits = k_fold_number)
+    j = 0
+    MSE_array = np.zeros(k_fold)
+
+    for train_indx, test_indx in kfold.split(X):
+        X_train = X[train_inds]
+        z_train = z[train_inds]
+
+        X_test = x[test_inds]
+        z_test = z[test_inds]
+
+        beta_OLS = OLS_regression(X_train, z_train)
+        z_pred = (X_test @ beta_OLS).ravel()
+        MSE_array[j] = MSE(z_test, z_pred)
 
 
 
 if __name__ == "__main__":
 
-    cross_validation(X_train, X_test, z_train, z_test, k_fold_number)
+    cross_validation(N, z_noise, k_fold_number)
