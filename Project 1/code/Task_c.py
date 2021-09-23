3918e5ffc25ebd5cd16c13c05eec1618ab49b96e
William Hirst <bod_holthe@outlook.com>
2021-09-23 17:28:07 +0200

    Altered cross_validation to include ridge method.

--- Project 1/code/Task_c.py
+++ Project 1/code/Task_c.py
@@ -11,11 +11,12 @@ from matplotlib.ticker import MaxNLocator
 
 
 
-def cross_validation(X, z, k_fold_number, returnError = False):
+def cross_validation(X, z, k_fold_number, method ,returnError = False, lamba = 0):
     kfold = KFold(n_splits = k_fold_number)
     j = 0
     z_pred_arr = np.zeros((int(np.shape(X)[0]/k_fold_number), k_fold_number))
     MSE_arr = np.zeros(k_fold_number)
+
     for train_indx, test_indx in kfold.split(X):
         X_train = X[train_indx]
         z_train = z[train_indx]
@@ -24,10 +25,11 @@ def cross_validation(X, z, k_fold_number, returnError = False):
         z_test = z[test_indx]
 
         X_train, X_test = scale_design_matrix(X_train, X_test)
-
-        beta_OLS = OLS_regression(X_train, z_train)
-
-        z_pred = (X_test @ beta_OLS).ravel()
+        if method == "OLS":
+            beta = OLS_regression(X_train, z_train)
+        elif method == "Rigde":
+            beta = RIDGE_regression(X, y, lamda)
+        z_pred = (X_test @ beta).ravel()
         z_pred_arr[:, j] = z_pred
 
         MSE_arr[j] = MSE(z_test.ravel(), z_pred)
@@ -52,16 +54,16 @@ def compaire_CV_B(N, z_noise,n, B, k_fold_number):
 
         z_pred_B = bootstrap(X_train, X_test, z_train, z_test, B)
 
-        error_CV[i] = cross_validation(X, z, k_fold_number, returnError = True)
+        error_CV[i] = cross_validation(X, z, k_fold_number, "OLS" , returnError = True)
         error_B[i] = np.mean(np.mean( (z_test-z_pred_B)**2, axis = 1, keepdims = True  ))
         error_sklearn[i] = np.mean(-cross_val_score(ols, X, z, scoring='neg_mean_squared_error', cv= k_fold_number))
 
     n_arr = np.linspace(0,n,n+1)
 
     plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
-    plt.plot(n_arr, error_CV, label = "Cross validation")
+    plt.plot(n_arr, error_CV, label = "Cross validation ")
     plt.plot(n_arr, error_B, label = "Bootstrap")
-    plt.plot(n_arr, error_sklearn, label = "sklearn")
+    plt.plot(n_arr, error_sklearn, "--" ,label = "sklearn")
 
     ax = plt.gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True)) # Force integer ticks on x-axis
