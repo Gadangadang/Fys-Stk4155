702350dab43e0fb4464b7c314da4e6dd669729b7
Gadangadang <sakarias.frette@outlook.com>
2021-11-11 23:10:15 +0100

    Added plot validation for franke nn

--- Project 2/code/validation_NN.py
+++ Project 2/code/validation_NN.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 from NN_functions import plot_heatmap
 
 from NeuralNetwork import NeuralNetwork
+from matplotlib.ticker import MaxNLocator
 
 
 
@@ -173,40 +174,44 @@ def Franke_NN():
     lmbd = 10**(-4)
     eta = 0.1
     num_hidden_layers = np.arange(1, 20)
-    test_scores_train = np.zeros(len(num_hidden_layers))
-    test_scores_test = np.zeros(len(num_hidden_layers))
     """
-    for i, num in enumerate(num_hidden_layers):
-        NN = NeuralNetwork(X_train, Z_train,
-                           num,
-                           num_hidden_nodes,
-                           batch_size,
-                           eta,
-                           lmbd,
-                           gamma,
-                           seed,
-                           activation,
-                           cost_func,
-                           loss,
-                           callback=False)
+    num_nodes = np.arange(1, 10)
+    for node in num_nodes:
+        test_scores_train = np.zeros(len(num_hidden_layers))
+        test_scores_test = np.zeros(len(num_hidden_layers))
+        for i, num in enumerate(num_hidden_layers):
+            NN = NeuralNetwork(X_train, Z_train,
+                               num,
+                               node,
+                               batch_size,
+                               eta,
+                               lmbd,
+                               gamma,
+                               seed,
+                               activation,
+                               cost_func,
+                               loss,
+                               callback=False)
 
-        NN.train_network_stochastic(int(epochs))
-        test_scores_train[i] = NN.get_score(X_train, Z_train)
-        test_scores_test[i] = NN.get_score(X_test, Z_test)
+            NN.train_network_stochastic(int(epochs))
+            test_scores_train[i] = NN.get_score(X_train, Z_train)
+            test_scores_test[i] = NN.get_score(X_test, Z_test)
 
-    plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
-    plt.plot(num_hidden_layers, test_scores_train, label="Train MSE")
-    plt.plot(num_hidden_layers, test_scores_test, label="Test MSE")
-    plt.xlabel(r"$Hidden Layers$", fontsize=14)
-    plt.ylabel(r"$MSE$", fontsize=14)
-    plt.legend(fontsize=13)
-    plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
-    plt.savefig("../article/figures/mse_hidden_layers.pdf",
-                bbox_inches="tight")
-    plt.show()
-    """
-    num_nodes = np.arange(10, 101, 10)
-    num_layers = np.arange(1, 11)
+        plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
+        ax = plt.gca()
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.plot(num_hidden_layers, test_scores_train, label="Train MSE")
+        plt.plot(num_hidden_layers, test_scores_test, label="Test MSE")
+        plt.xlabel(r"$Hidden Layers$", fontsize=14)
+        plt.ylabel(r"$MSE$", fontsize=14)
+        plt.legend(fontsize=13)
+        plt.tight_layout(pad=1.1, w_pad=0.7, h_pad=0.2)
+        plt.savefig(f"../article/figures/mse_hidden_layers_node_{node}.pdf",
+                    bbox_inches="tight")
+        plt.show()
+
+    num_nodes = np.arange(41, 51)
+    num_layers = np.arange(2, 8)
     test_scores_NN_lay_node = np.zeros((len(num_layers), len(num_nodes)))
 
     #Nodes and layers
@@ -231,9 +236,13 @@ def Franke_NN():
 
     plot_heatmap(test_scores_NN_lay_node, [r"Number of nodes", num_nodes],
                  [r"Number of layers", num_layers], title="Grid search Neural Network", name=None)
+
     """
-    num_hidden_layers = 4
-    num_hidden_nodes = 50
+
+
+    num_hidden_layers = 3
+    num_hidden_nodes = 42
+    activation = "sigmoid"
     NN = NeuralNetwork(X_train, Z_train,
                        num_hidden_layers,
                        num_hidden_nodes,
@@ -250,7 +259,27 @@ def Franke_NN():
     NN.train_network_stochastic(int(epochs))
     print("MSE: {}".format(NN.MSE_score(X_test, Z_test)))
     print("R2: {}".format(NN.R2_score(X_test, Z_test)))
-    """
+
+
+    N = 1000
+    x_val = np.random.uniform(0, 1, N)
+    y_val = np.random.uniform(0, 1, N)
+
+    X_validation = np.array([x_val, y_val]).T
+
+    # exit()
+    z_val = FrankeFunction(x_val, y_val) + noise * np.random.randn(N)
+
+    X_validation, x_val, y_val = mean_scale(X_validation, x_val, y_val)
+
+    prediction = NN.predict(X_validation)
+    print(prediction.shape)
+
+    plot_3D_shuffled("Neural Net prediction on Franke's func",
+                     x_val, y_val, prediction.ravel(), "Prediction", "test.pdf", show = True, save = True)
+
+    plot_3D_shuffled("Franke's function data with noise",
+                     x_val, y_val, z_val, "Actual data", "act_data.pdf", show = True, save = True)
 
 
 def sklearn_NN(X, y, eta, lmbd, epochs, num_hidden_layers,
@@ -356,6 +385,7 @@ if __name__ == "__main__":
     path += '/../../Project 1/code'
     sys.path.append(path)
     from Functions import *
+    from prediction_plots import plot_3D_shuffled
     # logic_gates_NN()
     # logic_gates_OLS()
     # tensorflow_logic_gates()
