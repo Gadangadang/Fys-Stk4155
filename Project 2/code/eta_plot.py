dd0f30e4d7a98db67a2da3b68955d3fa9437d752
Gadangadang <sakarias.frette@outlook.com>
2021-11-12 14:33:34 +0100

    Fixed amplitude for eta decay
    Now, eta decay is always eta_0 in the beginning

---
+++ Project 2/code/eta_plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.ticker as tkr
+import matplotlib.pyplot as plt
+from NeuralNetwork import NeuralNetwork
+
+def sigmoid_activation(x):
+    return 1/(1+np.exp(-x))
+
+
+epochs = np.arange(0, 21)
+eta_0 = 0.1
+dropp_time=5
+A = 1
+k = 0.5
+
+def eta_func(k, epoch):
+    return eta_0 * A*sigmoid_activation(k*(dropp_time-epoch))
+
+
+eta_vals = eta_func(k, epochs)
+print(eta_vals)
+plt.plot(epochs, eta_vals)
+plt.show()
