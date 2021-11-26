8d9b6ea9f978cc410e3f82a5a00dd688c9b9931d
William Hirst <bod_holthe@outlook.com>
2021-11-26 13:22:15 +0100

    Added a call func and made x and t to tensors in init.

--- Project 3/code/ML_PDE_solver.py
+++ Project 3/code/ML_PDE_solver.py
@@ -11,13 +11,18 @@ class PDE_ml_solver:
     def __init__(self, L, T, dx, dt, epochs, I):
         Nx = int(L / dx)
         Nt = int(T / dt)
-        self.x = np.linspace(0, L, Nx)
-        self.t = np.linspace(0, T, Nt)
+        self.x = tf.convert_to_tensor(np.linspace(0, L, Nx), dtype="float32")
+        self.t = tf.convert_to_tensor(np.linspace(0, T, Nt), dtype="float32")
         self.I = I
         self.num_epochs = epochs
         self.g_t_jacobian_func = jacobian(self.g_trial, 0)
         self.g_t_hessian_func = hessian(self.g_trial, 0)
         self.optimizer = optimizers.SGD(learning_rate=0.01)
+
+    def __call__(self):
+        pred = self.g_trial(self.model, self.x,self.t)
+        return pred
+
 
     def get_model(self):
         model = tf.keras.Sequential(
@@ -25,32 +30,24 @@ class PDE_ml_solver:
                 tf.keras.layers.Dense(10, activation="sigmoid", input_shape=(2,)),
                 tf.keras.layers.Dense(10, activation="sigmoid"),
                 tf.keras.layers.Dense(1, activation="sigmoid"),
-            ]
-        )
-        # model.compile(loss = "cost_function", optimizer="adam", metrics=["MSE"])
+            ])
         return model
 
     def tf_run(self):
         train_loss_results = []
-        train_accuracy_results = []
         model = self.get_model()
         epoch_loss_avg = tf.keras.metrics.Mean()
-        epoch_accuracy = tf.keras.metrics.MeanSquaredError()
 
         for epoch in range(self.num_epochs):
             loss_value, grads = self.grad(model)
             self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
             # Track progress
             epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-            # epoch_accuracy.update_state(y, model(x, training=True))
-
             train_loss_results.append(epoch_loss_avg.result())
-            # train_accuracy_results.append(epoch_accuracy.result())
-        return train_loss_results  # , train_accuracy_results
+        self.model = model
+        return train_loss_results
 
     def grad(self, model):
-
         with tf.GradientTape() as tape:
             loss_value = self.cost_function(model)
             t_grad = tape.gradient(loss_value, model.trainable_variables)
@@ -61,16 +58,9 @@ class PDE_ml_solver:
         """
         Calculate derivatives.
         """
-        x, t = tf.convert_to_tensor(self.x, dtype="float32"), tf.convert_to_tensor(
-            self.t, dtype="float32"
-        )
+        x, t = tf.convert_to_tensor(self.x, dtype="float32"), \
+        tf.convert_to_tensor(self.t, dtype="float32")
 
-        """with tf.GradientTape(persistent=True) as tape1:
-            tape1.watch(x)
-            with tf.GradientTape(persistent=True) as tape2:
-                tape1.watch([x,t])
-                g_trial = self.g_trial(model)
-            """
         with tf.GradientTape(persistent=True) as tape1:
             tape1.watch(x)
             tape1.watch(t)
@@ -88,12 +78,9 @@ class PDE_ml_solver:
         g_trial(x, t) = h_1(x, t) + h_2(x,t)N(x,t,P)
         h_1 and h_2 are functions to control boundary and inital conditions
         """
-        # x, t  = tf.convert_to_tensor(x, dtype="float32"), tf.convert_to_tensor(t, dtype="float32")
         XT = tf.stack([t, x], axis=1)
-        # point = np.array([self.x,self.t]).reshape(1,2)
-        return (1 - self.t) * self.I(self.x) + self.x * (1 - self.x) * self.t * model(
-            XT, training=True
-        )
+        return (1 - self.t) * self.I(self.x) + \
+        self.x * (1 - self.x) * self.t * model(XT, training=True)
 
 
 def g_analytic(x, t):
@@ -113,12 +100,17 @@ if __name__ == "__main__":
     dx = 1 / 10
     dt = 5 / 1000  # 0.5*dx**2
 
-    epochs = 5000
+    epochs = 100
     ML = PDE_ml_solver(L, T, dx, dt, epochs, I)
     loss = ML.tf_run()
 
-    plt.plot(np.arange(epochs), loss)
+    u = ML()
+    x = np.linspace(0,L, int(L/dx))
+    plt.plot(x, u[-1])
     plt.show()
+    #plt.plot(np.arange(epochs), loss)
+    #plt.show()
+
 
     print("TensorFlow version: {}".format(tf.__version__))
     print("Eager execution: {}".format(tf.executing_eagerly()))
