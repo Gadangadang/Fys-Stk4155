646ee624225eb8b56df6bc576975eb09730ce879
William Hirst <bod_holthe@outlook.com>
2021-11-26 18:02:17 +0100

    Changed from tensors to dataset and added batch structure.
    Also changed so that the solution is calculated one time step at a time.

--- Project 3/code/ML_PDE_solver.py
+++ Project 3/code/ML_PDE_solver.py
@@ -6,15 +6,15 @@ from autograd import jacobian, hessian, grad
 
 from tensorflow.keras import optimizers, regularizers
 from tensorflow.keras.layers import Dense, Input
+from tensorflow.data import Dataset
 from tqdm import tqdm
 
 
 class PDE_ml_solver:
-    def __init__(self, L, T, dx, dt, epochs, I):
-        Nx = int(L / dx)
-        Nt = int(T / dt)
-        self.x = tf.convert_to_tensor(np.linspace(0, L, Nx), dtype="float32")
-        self.t = tf.convert_to_tensor(np.linspace(0, T, Nt), dtype="float32")
+    def __init__(self, L, T, dx, dt, epochs, I, batch_size):
+        self.x = tf.cast(tf.linspace(0, L, int(L / dx)), tf.float32)
+        self.t = tf.cast(tf.linspace(0., T, int(T / dt)), tf.float32)
+        self.dataset = self.create_dataset(self.x,self.t,batch_size)
         self.I = I
         self.num_epochs = epochs
         self.g_t_jacobian_func = jacobian(self.g_trial, 0)
@@ -22,8 +22,20 @@ class PDE_ml_solver:
         self.optimizer = optimizers.SGD(learning_rate=0.01)
 
     def __call__(self):
-        pred = self.g_trial(self.model, self.x, self.t)
-        return pred
+        sol = []
+        #pred = self.g_trial(self.model, self.x, self.t)
+        for i, ti in enumerate(self.t):
+            sol.append([])
+            for xi in self.x:
+                sol[i].append(self.g_trial(self.model, tf.Variable([xi]), tf.Variable([ti]))[0][0].numpy())
+
+        return sol
+
+    def create_dataset(self,x,t,batch_size):
+        data = tf.stack([tf.random.shuffle(t),tf.random.shuffle(x)], axis=1)#Dataset.zip((t, x))
+        data = Dataset.from_tensor_slices(data)
+        data = data.batch(batch_size)
+        return data
 
     def get_model(self):
         model = tf.keras.Sequential(
@@ -41,41 +53,45 @@ class PDE_ml_solver:
         epoch_loss_avg = tf.keras.metrics.Mean()
 
         for epoch in tqdm(range(self.num_epochs)):
-            loss_value, grads = self.grad(model)
-            self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            # Track progress
-            epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-            train_loss_results.append(epoch_loss_avg.result())
+            loss_epoch = 0
+            for step, batch in enumerate(self.dataset):
+                self.batch = batch
+                loss_value, grads = self.grad(model)
+                self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+                # Track progress
+                #epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+                #train_loss_results.append(epoch_loss_avg.result())
+                train_loss_results.append(tf.math.reduce_mean(loss_value))
         self.model = model
         return train_loss_results
 
     def grad(self, model):
         with tf.GradientTape() as tape:
             loss_value = self.cost_function(model)
-        t_grad = tape.gradient(loss_value, model.trainable_variables)
-        del tape
+            t_grad = tape.gradient(loss_value, model.trainable_variables)
+            del tape
         return loss_value, t_grad
 
     def cost_function(self, model):
         """
         Calculate derivatives.
         """
-        x, t = tf.convert_to_tensor(self.x, dtype="float32"), tf.convert_to_tensor(
-            self.t, dtype="float32"
-        )
-
+        t,x = self.batch[:,0], self.batch[:,1]
         with tf.GradientTape(persistent=True) as tape1:
             tape1.watch(x)
-            tape1.watch(t)
-            g_trial = self.g_trial(model, x, t)
-            g_x = tape1.gradient(g_trial, x)
-            g_xx = tape1.gradient(g_x, x)
 
-        g_t = tape1.gradient(g_trial, t)
-        residual = g_xx - g_t
-        MSE = tf.reduce_mean(residual ** 2)
+            with tf.GradientTape() as tape2:
+                tape2.watch(t)
+                g_trial = self.g_trial(model, x, t)
+                g_x = tape1.gradient(g_trial, x)
+
+        g_xx = tape1.gradient(g_x, x)
+        g_t = tape2.gradient(g_trial, t)
         del tape1
-        return MSE
+        del tape2
+        residual = (g_xx - g_t)
+        #MSE = residual
+        return tf.math.abs(residual)
 
     def g_trial(self, model, x, t):
         """
@@ -83,8 +99,8 @@ class PDE_ml_solver:
         h_1 and h_2 are functions to control boundary and inital conditions
         """
         XT = tf.stack([t, x], axis=1)
-        h1 = (1 - self.t) * self.I(self.x)
-        h2 = self.x * (1 - self.x) * self.t
+        h1 = (1 - t) * self.I(x)
+        h2 = x * (1 - x) * t
         return h1 + h2 * model(XT, training=True)
 
 
@@ -107,17 +123,24 @@ if __name__ == "__main__":
     L = 10
     T = 0.5
     dx = 1 / 10
-    dt = 5 / 1000  # 0.5*dx**2
+    dt = 0.005  # 0.5*dx**2
 
-    epochs = 1000
-    ML = PDE_ml_solver(L, T, dx, dt, epochs, I)
+
+    epochs = 500
+    ML = PDE_ml_solver(L, T, dx, dt, epochs, I, 50)
     loss = ML.tf_run()
 
     u = ML()
     x = np.linspace(0, L, int(L / dx))
     t = np.linspace(0, T, int(T / dt))
-    plt.plot(x, u[-1])
-    plt.plot(x,g_analytic(x,T), "--")
+    for i in range(0,100,10):
+        plt.plot(x, u[i])
+        plt.plot(x,g_analytic(x,dt*i), "--")
+
+
+    #plt.plot(x,g_analytic(x,dt), "--")
+    #plt.xlim([0,1])
     plt.show()
-    # plt.plot(np.arange(epochs), loss)
-    # plt.show()
+    """plt.plot(np.arange(len(loss)), loss)
+    plt.show()
+    """
