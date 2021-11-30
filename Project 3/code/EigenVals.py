d502bd2326b8d5c5ad118c219ff1983200fc5c3e
William Hirst <bod_holthe@outlook.com>
2021-11-30 13:40:06 +0100

    Tweaks params.

--- Project 3/code/EigenVals.py
+++ Project 3/code/EigenVals.py
@@ -24,7 +24,7 @@ class EigenVal(NeuralNetworkPDE):
         X = self.model(self.t)[-1]
         X_T = tf.transpose(X)
         X_T_X = tf.reduce_sum(X_T * X)
-        AX = tf.linalg.matvec(tf.cast(A,tf.float32), X)
+        AX = tf.linalg.matvec(tf.cast(self.A,tf.float32), X)
         lmb =  tf.reduce_sum(X_T * AX) / (X_T_X)
         return lmb
 
@@ -36,7 +36,7 @@ class EigenVal(NeuralNetworkPDE):
             X =  model(t, training=True)
         X_dt = tape.gradient(X,t)
         del tape
-
+        A = self.A
         X_T = tf.transpose(X)
         AX = tf.einsum("ij,kj->ki", A,X)
         LS = self.XX_0 *AX
@@ -63,11 +63,11 @@ if __name__ == "__main__":
     Q = np.random.rand(6,6)
     A = (Q.T + Q)/2
 
-    T = 1e30
+    T = 1e10
     Nt = 100
     t = np.linspace(0,T, Nt).reshape(Nt,1)
     epochs = 10000
-    lr = 0.0001
+    lr = 0.001
     #check out einsum.
 
     EV = EigenVal(t, epochs,  lr, A)
