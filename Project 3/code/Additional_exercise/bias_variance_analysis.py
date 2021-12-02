37ae26ea255177cf684c640e76d877ab50a561a5
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2021-12-02 13:56:28 +0100

    done with all three methods, just need to run some simulations

--- Project 3/code/Additional_exercise/bias_variance_analysis.py
+++ Project 3/code/Additional_exercise/bias_variance_analysis.py
@@ -11,6 +11,12 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from plot_set import *
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+import tensorflow as tf
+
+
 
 
 class bias_variance():
@@ -21,15 +27,21 @@ class bias_variance():
         self.y_test = y_test
 
 
-    def set_method(self, method, complexity_array):
-        methods = {"decision_tree": self.decision_tree_model, "boosting": self.boosting_model}
-        complexity_labels = {"decision_tree": "tree depth", "boosting": "number of estimators"}
-        self.complexity_label = complexity_labels[method]
-        self.method = methods[method]
+    def set_method(self, method, complexity_array, *args):
+        methods =   {   "decision_tree":      [self.decision_tree_model   ,"tree depth"],
+                        "boosting":           [self.boosting_model        ,"# estimators"],
+                        "NN_variable_nodes":  [self.NN_model_nodes             ,"# hidden nodes"],
+                        "NN_variable_layers": [self.NN_model_layers              ,"# hidden layers"]}
+
+        self.method, self.complexity_label = methods[method]
         self.n_arr = complexity_array
         self.bias = np.zeros(len(self.n_arr))
         self.variance = np.zeros(len(self.n_arr))
         self.MSE_test = np.zeros(len(self.n_arr))
+        self.args = args
+        self.fit = lambda X, y: self.model.fit(X, y)
+
+
 
 
     def decision_tree_model(self, complexity):
@@ -38,15 +50,37 @@ class bias_variance():
     def boosting_model(self, complexity):
         return AdaBoostRegressor(random_state=0, n_estimators=complexity)
 
+    def NN_model_nodes(self, complexity):
+        num_hidden_nodes = complexity
+        num_hidden_layers = self.args[0]
+        return self.feed_forward_network(num_hidden_nodes, num_hidden_layers)
 
-    def feed_forward_network():
-        pass
+
+    def NN_model_layers(self, complexity):
+        num_hidden_nodes = self.args[0]
+        num_hidden_layers = complexity
+        return self.feed_forward_network(num_hidden_nodes, num_hidden_layers)
+
+
+    def feed_forward_network(self, num_hidden_nodes, num_hidden_layers):
+        epochs = 12
+        model = Sequential()
+        model.add(Input(shape=self.X_train.shape[1]))
+        for i in range(num_hidden_layers):
+            model.add(Dense(num_hidden_nodes))
+        model.add(Dense(1))
+        model.compile(optimizer='sgd', loss='mse')
+        self.fit = lambda X, y: self.model.fit(X, y, epochs = epochs ,verbose = 0)
+        return model
+
+
+
 
     def bootstrap(self, num_bootstraps):
         y_pred = np.empty((y_test.shape[0], num_bootstraps))
         for b in range(num_bootstraps):
             X_, y_ = resample(self.X_train, self.y_train)
-            self.model.fit(X_, y_.ravel())
+            self.fit(X_, y_.ravel())
             y_pred[:, b] = self.model.predict(X_test).ravel()
         return y_pred
 
@@ -75,9 +109,6 @@ class bias_variance():
 
 
 
-
-
-
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = fetch_data()
     n_arr = np.arange(1, 15)
@@ -89,4 +120,14 @@ if __name__ == "__main__":
 
     bv_object.set_method("boosting", n_arr)
     bv_object.analyze()
+    bv_object.plot()
+
+    n_arr = np.linspace(1, 100, 10).astype("int")
+    bv_object.set_method("NN_variable_nodes", n_arr, 2)
+    bv_object.analyze(10)
+    bv_object.plot()
+
+    n_arr = np.linspace(1, 200, 10).astype("int")
+    bv_object.set_method("NN_variable_layers", n_arr, 20)
+    bv_object.analyze(10)
     bv_object.plot()
